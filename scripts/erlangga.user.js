77cd7912cc51161dd96fdb8b2373d3f7fb479af4
Dave Kendrick Turmawan <dave.turmawan@outlook.com>
2026-03-26 17:21:31 +0700

    remove hardcoding of .mp3

--- scripts/erlangga.user.js
+++ scripts/erlangga.user.js
@@ -1,22 +1,28 @@
 // ==UserScript==
 // @name         Erlangga URL Rewriter
 // @namespace    https://dave9123.me/
-// @version      1.0
+// @version      1.1
 // @description  Rewrite Erlangga QR codes into direct links
 // @author       dave9123
 // @match        https://media.erlanggaonline.co.id/*
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-const url = new URL(window.location.href);
-const id = url.searchParams.get("id");
+(function () {
+    'use strict';
 
-if (!id) return;
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get('id');
+    if (!id) return;
 
-const base = id.split(".")[0];
+    const base = id.split('.')[0];
+    if (!base) return;
 
-const newUrl = `https://media.erlanggaonline.co.id/data/${base}/${id}.mp3`;
+    const newUrl = new URL(url.origin);
+    newUrl.pathname = `/data/${encodeURIComponent(base)}/${encodeURIComponent(id)}`;
 
-if (window.location.href !== newUrl) {
-    window.location.replace(newUrl);
-}
+    if (window.location.href !== newUrl.href) {
+        window.location.replace(newUrl.href);
+    }
+})();
